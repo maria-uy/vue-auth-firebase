966c29b903395a30a45d53c40d43c11c0408d0bd
María <maria.farcilli@gmail.com>
2023-08-07 12:42:18 +0200

    DEL firebase key

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 const authApi = axios.create({
     baseURL: 'https://identitytoolkit.googleapis.com/v1/accounts',
     params: {
-        key: 'AIzaSyDQl7A9-EtkvchCpp2zAjJNo4WVZKTDDQA'
+        key: ''
     }
 })
 
